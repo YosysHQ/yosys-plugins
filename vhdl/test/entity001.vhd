210fbb963e5cee9b35bab7bff0af516c8ca6b1db
Sebastian Kuzminsky <seb@highlab.com>
2016-03-26 23:05:25 -0600

    start adding some sanity tests

---
+++ vhdl/test/entity001.vhd
@@ -0,0 +1,6 @@
+entity e1 is
+    Port (
+        a: in  STD_LOGIC;
+        b: out  STD_LOGIC
+    );
+end e1;
---
+++ vhdl/test/entity002.vhd
@@ -0,0 +1,7 @@
+entity e1 is
+    Port (
+        a: in  STD_LOGIC;
+        b,c,d: out  STD_LOGIC;
+        e,f: in  STD_LOGIC
+    );
+end e1;
